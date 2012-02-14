4ff75d973f035f7cf915f0bdcabf575d7c2362cf
Kaleu Caminha <kaleu.caminha@gmail.com>
2012-02-13 22:48:53 -0200

    Criado projeto no NetBeans para o jogo com um Ol Mundo. Bem Vindo ao Nhock

---
+++ Nhock-Project/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Responsabilidade da Classe: Iniciar o Software
+ * @author KALEU
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        NhockGame game = new NhockGame();
+		game.init();
+    }
+
+}
---
+++ Nhock-Project/src/NhockGame.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ * Responsabilidade da Classe: Executar o loop do Jogo com os objetos principais.
+ * @author KALEU
+ */
+public class NhockGame {
+
+	public NhockGame(){
+		//Inicializa objetos como cenário, jogador, etc.
+	}
+
+	public void init(){
+		//executa loop do jogo
+			//Pega eventos
+			//Processa
+			//Desenha
+
+		//Por enquanto, apenas exibe um Hello World para que possamos acompanhar o desenvolvimento.
+		JOptionPane.showMessageDialog(null, "Você está pronto para comer o máximo que puder?", "Nhock.!", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
